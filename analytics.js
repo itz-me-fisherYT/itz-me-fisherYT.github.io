8c4f20273eec681805c2bed2b5d4ee19cf472433
itz-me-fisherYT <itz.me.fisheryt@gmail.com>
2025-11-21 21:15:21 +1000

    Delete analytics.js

--- analytics.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    let visitCount = localStorage.getItem('visitCount') || 0;
-    visitCount = parseInt(visitCount) + 1;
-    localStorage.setItem('visitCount', visitCount);
-    console.log(`Visit #${visitCount}`);
-})();
